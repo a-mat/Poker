a1dc0f712dcd9014f0e927bf994eae899afb5188
June <aomathew94@gmail.com>
2017-05-26 01:27:09 -0400

    JavaDoc in Cards and Decks. Comparable removed from Cards

--- src/Cards.java
+++ src/Cards.java
@@ -1,5 +1,13 @@
 
-public class Cards implements Comparable<Cards> {
+/*
+ *
+ * The card class defines the state of the cards in Poker. Each card contains a rank and a suit.
+ *
+ *
+ */
+
+
+public class Cards  {
 	private String rank;
 	private String suit;
 	public  String getRank() {
@@ -11,7 +19,7 @@ public class Cards implements Comparable<Cards> {
 	public void setRank(String rank) {
 		this.rank = rank;
 	}
- 
+
 
 	public String getSuit() {
 		return suit;
@@ -36,7 +44,18 @@ public class Cards implements Comparable<Cards> {
 
 	}
 
-
+/*
+ * A final string array of all the possible ranks
+ * The royal cards have been given number value instead of their name for ease of this project.
+ * Comparable puts 10 before 2-9 due to the first digit. Because of this all the numbers before 10 have been
+ * given a zero value infront of it to preserve order.
+ */
+	private final static String[] ranks = {"02","03","04","05","06","07","08","09","10","11","12",
+			"13","14"};
+	/*
+	 * toString have been overridden so that numbers 02-09 can be read as 2-9 by removing the first zero.
+	 * Royal cards are also translated from 11-14 as Jack to Ace.
+	 */
 
 	@Override
 	public String toString() {
@@ -48,14 +67,13 @@ public class Cards implements Comparable<Cards> {
 		 else return rank.substring(1)+" of "+ suit;
 	}
 
-
-
-	private final static String[] ranks = {"02","03","04","05","06","07","08","09","10","11","12",
-			"13","14"};
 	public static String[] getRanks(){
 		return ranks;
 	}
 
+	/*
+	 * the suits are put in an enum to be read later as there are only four possible suits.
+	 */
 	enum suits {
 		SPADE,
 		HEART,
@@ -63,11 +81,5 @@ public class Cards implements Comparable<Cards> {
 		CLUB;
 	}
 
-	@Override
-	public int compareTo(Cards o) {
 
-		int n=getRank().compareTo(o.rank);
-		if(n==0) return o.suit.compareTo(getSuit());
-		else return n;
-	}
 }
